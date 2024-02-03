fd0f5a74c1d4683aa659e7877fc66ca60012e76f
GAKUTO_HOME\gakuto <gakuto_winHome>
2024-02-04 08:20:21 +0900

    簡易フォームを用いた値の受け渡し

--- SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
@@ -5,35 +5,11 @@ namespace SampleRazorApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public string Message { get; set; }
-        private string[][] data = new string[][] 
-        {
-        new string[]{"Taro", "taro@yamada"},
-        new string[]{"Hanako", "hanako@flower"},
-        new string[]{"Sachiko", "sachiko@happy"}
-        };
-
-        [BindProperty(SupportsGet = true)]
-        public int Id { get; set; }
+        public string Message { get; set; } = "sample message";
 
         public void OnGet()
         {
-            Message = "これはMessageプロパティの値です。";
+            Message = "何か書いて下さい。";
         }
-
-        public string GetData(int id)
-        {
-            string[] target = data[id];
-            return $"[名前：{target[0]}、メール：{target[1]}]";
-        }
-
-
     }
 }
