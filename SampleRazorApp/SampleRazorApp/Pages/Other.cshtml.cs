19018af2bfcc1348be3d3b2171e01d4f9df21ef4
GAKUTO_HOME\gakuto <gakuto_winHome>
2024-02-04 08:45:04 +0900

    @Html.Editorによるフィールド生成

--- SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
@@ -3,13 +3,39 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
-    public class OtherModel : PageModel
+    public enum Gender
     {
-        public string Message { get; set; }
+        male,
+        female
+    }
 
-        public void OnPost()
+    public enum Platform
+    {
+        Windows,
+        macOS,
+        Linux,
+        ChromeOS,
+        Android,
+        iOS
+    }
+
+    public class OtherModel : PageModel
+    { 
+        public string? Message { get; set; }
+
+        public bool Check {  get; set; }
+        public Gender Gender { get; set; }
+        public Platform Platform { get; set; }
+        public Platform[] Platforms { get; set; }
+
+        public void OnGet()
         {
-            Message = "you typed:" + Request.Form["msg"];
+            Message = "check & sekect it!";
+        }
+
+        public void OnPost(bool check, string gender, Platform pc, Platform[] pc2)
+        {
+            Message = "Result: " + check + "," + gender + "," + pc + ", " + pc2.Length;
         }
     }
 }
