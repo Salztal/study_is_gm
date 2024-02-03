4fb72edab5f393461edf1594296c7d074df2bf8e
GAKUTO_HOME\gakuto <gakuto_winHome>
2024-02-04 08:26:33 +0900

    フォームとページモデルの関連付け

--- SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
@@ -1,15 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleRazorApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public string Message { get; set; } = "sample message";
+        public string Message { get; set; } = "no message";
+
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Tel {  get; set; }
 
         public void OnGet()
         {
-            Message = "何か書いて下さい。";
+            Message = "入力してください";
+        }
+
+        public void OnPost(string name, string password, string mail, string tel)
+        {
+            Message = "[Name: " + name + ", password:(" + password.Length
+                + " chars), mail:" + mail + " <" + tel + ">]";
         }
     }
 }
