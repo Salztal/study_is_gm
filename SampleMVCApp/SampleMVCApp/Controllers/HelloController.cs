5fdb40aa69d96c2a2c9f151a2fb8d07f1fc5a9e7
GAKUTO_HOME\gakuto <gakuto_winHome>
2024-02-04 17:58:23 +0900

    複数項目受け渡し

--- SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
+++ SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
@@ -6,14 +6,21 @@ namespace SampleMVCApp.Controllers
     {
         public IActionResult Index()
         {
-            ViewData["Message"] = "Hello! this is sample message!";
+            ViewData["Message"] = "Input your data:";
+            ViewData["name"] = "";
+            ViewData["mail"] = "";
+            ViewData["tel"] = "";
             return View();
         }
 
         [HttpPost]
-        public IActionResult Form(string msg)
+        public IActionResult Form(string name, string mail, string tel)
         {
-            ViewData["Message"] = msg;
+            ViewData["name"] = name;
+            ViewData["mail"] = mail;
+            ViewData["tel"] = tel;
+            ViewData["message"] = ViewData["name"] + ", " +
+                    ViewData["mail"] + ",  " + ViewData["tel"];
             return View("Index");
         }
     }
