1f4200d0abb8e2ae798c38157d737765a9e8aad0
GAKUTO_HOME\gakuto <gakuto_winHome>
2024-02-04 17:47:44 +0900

    formを用いた値の受け渡し

--- SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
+++ SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
@@ -6,7 +6,15 @@ namespace SampleMVCApp.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Message"] = "Hello! this is sample message!";
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(string msg)
+        {
+            ViewData["Message"] = msg;
+            return View("Index");
         }
     }
 }
